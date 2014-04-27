411135453466a8f5373c2155cb0d8c46ba6a223c
Stefan Majer <stefan.majer@gmail.com>
2014-04-27 18:30:15 +0200

    first working version commited.

---
+++ src/main/java/org/influxdb/InfluxDB.java
@@ -0,0 +1,456 @@
+package org.influxdb;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.io.CharStreams;
+
+import retrofit.ErrorHandler;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.DELETE;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * A Interface to a InfluxDB Database.
+ * 
+ * @author stefan.majer [at] gmail.com
+ * 
+ */
+public class InfluxDB {
+
+	private final String username;
+	private final String password;
+	private final RestAdapter restAdapter;
+	private final InfluxDBService influxDBService;
+
+	public static InfluxDB connect(final String url, final String username, final String password) {
+		Preconditions.checkArgument(!Strings.isNullOrEmpty(url), "The URL may not be null or empty.");
+		Preconditions.checkArgument(!Strings.isNullOrEmpty(username), "The username may not be null or empty.");
+		return new InfluxDB(url, username, password);
+	}
+
+	private InfluxDB(final String url, final String username, final String password) {
+		super();
+		this.username = username;
+		this.password = password;
+
+		this.restAdapter = new RestAdapter.Builder()
+				.setEndpoint(url)
+				.setErrorHandler(new InfluxDBErrorHandler())
+				.build();
+		this.influxDBService = this.restAdapter.create(InfluxDBService.class);
+	}
+
+	/** Controls the level of logging. */
+	public enum LogLevel {
+		/** No logging. */
+		NONE,
+		/** Log only the request method and URL and the response status code and execution time. */
+		BASIC,
+		/** Log the basic information along with request and response headers. */
+		HEADERS,
+		/**
+		 * Log the headers, body, and metadata for both requests and responses.
+		 * <p>
+		 * Note: This requires that the entire request and response body be buffered in memory!
+		 */
+		FULL;
+	}
+
+	public InfluxDB setLogLevel(final LogLevel logLevel) {
+		switch (logLevel) {
+		case NONE:
+			this.restAdapter.setLogLevel(retrofit.RestAdapter.LogLevel.NONE);
+			break;
+		case BASIC:
+			this.restAdapter.setLogLevel(retrofit.RestAdapter.LogLevel.BASIC);
+			break;
+		case HEADERS:
+			this.restAdapter.setLogLevel(retrofit.RestAdapter.LogLevel.HEADERS);
+			break;
+		case FULL:
+			this.restAdapter.setLogLevel(retrofit.RestAdapter.LogLevel.FULL);
+			break;
+		default:
+			break;
+		}
+		return this;
+	}
+
+	public Pong ping() {
+		return this.influxDBService.ping();
+	}
+
+	public void write(final String database, final Serie[] series, final TimeUnit precision) {
+		this.influxDBService.write(database, series, this.username, this.password, toTimePrecision(precision));
+	}
+
+	public List<Serie> Query(final String database, final String query, final TimeUnit precision) {
+		return this.influxDBService.query(database, query, this.username, this.password, toTimePrecision(precision));
+	}
+
+	public String createDatabase(final String name, final int replicationFactor) {
+		Database db = new Database(name, replicationFactor);
+		String response = this.influxDBService.createDatabase(db, this.username, this.password);
+		if (null == response) {
+			return "ok";
+		}
+		return response;
+	}
+
+	public String deleteDatabase(final String name) {
+		String response = this.influxDBService.deleteDatabase(name, this.username, this.password);
+		if (null == response) {
+			return "ok";
+		}
+		return response;
+	}
+
+	public List<Database> describeDatabases() {
+		return this.influxDBService.describeDatabases(this.username, this.password);
+	}
+
+	public String createClusterAdmin(final String name, final String password) {
+		User user = new User();
+		user.setName(name);
+		user.setPassword(password);
+		return this.influxDBService.createClusterAdmin(user, this.username, this.password);
+	}
+
+	public String deleteClusterAdmin(final String name) {
+		return this.influxDBService.deleteClusterAdmin(name, this.username, this.password);
+	}
+
+	public List<User> describeClusterAdmins() {
+		return this.influxDBService.describeClusterAdmins(this.username, this.password);
+	}
+
+	public String updateClusterAdmin(final String name, final String password) {
+		User user = new User();
+		user.setPassword(password);
+		return this.influxDBService.updateClusterAdmin(user, name, this.username, this.password);
+	}
+
+	public String createDatabaseUser(final String database, final String name, final String password,
+			final String... permissions) {
+		User user = new User();
+		user.setName(name);
+		user.setPassword(password);
+		user.setPermissions(permissions);
+		return this.influxDBService.createDatabaseUser(database, user, this.username, this.password);
+	}
+
+	public String deleteDatabaseUser(final String database, final String name) {
+		return this.influxDBService.deleteDatabaseUser(database, name, this.username, this.password);
+	}
+
+	public List<User> describeDatabaseUsers(final String database) {
+		return this.influxDBService.describeDatabaseUsers(database, this.username, this.password);
+	}
+
+	public String updateDatabaseUser(final String database, final String name, final String password,
+			final String... permissions) {
+		User user = new User();
+		user.setPassword(password);
+		user.setPermissions(permissions);
+		return this.influxDBService.updateDatabaseUser(database, user, name, this.username, this.password);
+	}
+
+	public String alterDatabasePrivilege(final String database, final String name, final boolean isAdmin,
+			final String... permissions) {
+		User user = new User();
+		user.setAdmin(isAdmin);
+		user.setPermissions(permissions);
+		return this.influxDBService.updateDatabaseUser(database, user, name, this.username, this.password);
+	}
+
+	public String authenticateDatabaseUser(final String database, final String username, final String password) {
+		return this.influxDBService.authenticateDatabaseUser(database, username, password);
+	}
+
+	public List<ContinuousQuery> getContinuousQueries(final String database) {
+		return this.influxDBService.getContinuousQueries(database, this.username, this.password);
+	}
+
+	public String deleteContinuousQuery(final String database, final int id) {
+		return this.influxDBService.deleteContinuousQuery(database, id, this.username, this.password);
+	}
+
+	public String deletePoints(final String database, final String serieName) {
+		// TODO implement
+		throw new IllegalArgumentException();
+	}
+
+	public String createScheduledDelete(final String database, final ScheduledDelete delete) {
+		// TODO implement
+		throw new IllegalArgumentException();
+	}
+
+	public List<ScheduledDelete> describeScheduledDeletes(final String database) {
+		// TODO implement
+		throw new IllegalArgumentException();
+	}
+
+	public String removeScheduledDelete(final String database, final int id) {
+		// TODO implement
+		throw new IllegalArgumentException();
+	}
+
+	private static String toTimePrecision(final TimeUnit t) {
+		switch (t) {
+		case SECONDS:
+			return "s";
+		case MILLISECONDS:
+			return "m";
+		case MICROSECONDS:
+			return "u";
+		default:
+			throw new IllegalArgumentException("time precision should be SECONDS or MILLISECONDS or MICROSECONDS");
+		}
+	}
+
+	public static class Pong {
+		private String status;
+
+		public String getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(final String status) {
+			this.status = status;
+		}
+	}
+
+	public static class ContinuousQuery {
+		private int id;
+		private String query;
+
+		public int getId() {
+			return this.id;
+		}
+
+		public void setId(final int id) {
+			this.id = id;
+		}
+
+		public String getQuery() {
+			return this.query;
+		}
+
+		public void setQuery(final String query) {
+			this.query = query;
+		}
+	}
+
+	public static class ScheduledDelete extends ContinuousQuery {
+	}
+
+	public static class Database {
+		private final String name;
+		private final int replicationFactor;
+
+		public Database(final String name, final int replicationFactor) {
+			super();
+			this.name = name;
+			this.replicationFactor = replicationFactor;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public int getReplicationFactor() {
+			return this.replicationFactor;
+		}
+
+	}
+
+	public static class User {
+		private String name;
+		private String password;
+		private boolean admin;
+		private String readFrom;
+		private String writeTo;
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(final String name) {
+			this.name = name;
+		}
+
+		public String getPassword() {
+			return this.password;
+		}
+
+		public void setPassword(final String password) {
+			this.password = password;
+		}
+
+		public boolean isAdmin() {
+			return this.admin;
+		}
+
+		public void setAdmin(final boolean admin) {
+			this.admin = admin;
+		}
+
+		public String getReadFrom() {
+			return this.readFrom;
+		}
+
+		public void setReadFrom(final String readFrom) {
+			this.readFrom = readFrom;
+		}
+
+		public String getWriteTo() {
+			return this.writeTo;
+		}
+
+		public void setWriteTo(final String writeTo) {
+			this.writeTo = writeTo;
+		}
+
+		public void setPermissions(final String... permissions) {
+			if (null != permissions) {
+				switch (permissions.length) {
+				case 0:
+					break;
+				case 2:
+					this.setReadFrom(permissions[0]);
+					this.setWriteTo(permissions[1]);
+					break;
+				default:
+					throw new IllegalArgumentException("You have to specify readFrom and writeTo permissions.");
+				}
+			}
+		}
+	}
+
+	public static class Serie {
+		private String name;
+		private String[] columns;
+		private Object[][] points;
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(final String name) {
+			this.name = name;
+		}
+
+		public String[] getColumns() {
+			return this.columns;
+		}
+
+		public void setColumns(final String[] columns) {
+			this.columns = columns;
+		}
+
+		public Object[][] getPoints() {
+			return this.points;
+		}
+
+		public void setPoints(final Object[][] points) {
+			this.points = points;
+		}
+
+	}
+
+	static class InfluxDBErrorHandler implements ErrorHandler {
+		@Override
+		public Throwable handleError(final RetrofitError cause) {
+			Response r = cause.getResponse();
+			if (r != null && r.getStatus() >= 400) {
+				try {
+					return new RuntimeException(CharStreams.toString(new InputStreamReader(
+							r.getBody().in(),
+							Charsets.UTF_8)));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return cause;
+		}
+	}
+
+	interface InfluxDBService {
+
+		@GET("/ping")
+		public Pong ping();
+
+		@POST("/db")
+		public String createDatabase(@Body Database database, @Query("u") String username, @Query("p") String password);
+
+		@DELETE("/db/{name}")
+		public String deleteDatabase(@Path("name") String name, @Query("u") String username, @Query("p") String password);
+
+		@GET("/db")
+		public List<Database> describeDatabases(@Query("u") String username, @Query("p") String password);
+
+		@POST("/db/{name}/series")
+		public String write(@Path("name") String name, @Body Serie[] series, @Query("u") String username,
+				@Query("p") String password, @Query("time_precision") String timePrecision);
+
+		@GET("/db/{name}/series")
+		public List<Serie> query(@Path("name") String name, @Query("q") String query, @Query("u") String username,
+				@Query("p") String password, @Query("time_precision") String timePrecision);
+
+		@POST("/cluster_admins")
+		public String createClusterAdmin(@Body User user, @Query("u") String username, @Query("p") String password);
+
+		@DELETE("/cluster_admins/{name}")
+		public String deleteClusterAdmin(@Path("name") String name, @Query("u") String username,
+				@Query("p") String password);
+
+		@GET("/cluster_admins")
+		public List<User> describeClusterAdmins(@Query("u") String username, @Query("p") String password);
+
+		@POST("/cluster_admins/{name}")
+		public String updateClusterAdmin(@Body User user, @Path("name") String name, @Query("u") String username,
+				@Query("p") String password);
+
+		@POST("/db/{database}/users")
+		public String createDatabaseUser(@Path("database") String database, @Body User user,
+				@Query("u") String username, @Query("p") String password);
+
+		@DELETE("/db/{database}/users/{name}")
+		public String deleteDatabaseUser(@Path("database") String database, @Path("name") String name,
+				@Query("u") String username, @Query("p") String password);
+
+		@GET("/db/{database}/users")
+		public List<User> describeDatabaseUsers(@Path("database") String database, @Query("u") String username,
+				@Query("p") String password);
+
+		@POST("/db/{database}/users/{name}")
+		public String updateDatabaseUser(@Path("database") String database, @Body User user, @Path("name") String name,
+				@Query("u") String username, @Query("p") String password);
+
+		@GET("/db/{database}/authenticate")
+		public String authenticateDatabaseUser(@Path("database") final String database, @Query("u") String username,
+				@Query("p") String password);
+
+		@GET("/db/{database}/continuous_queries")
+		public List<ContinuousQuery> getContinuousQueries(@Path("database") String database,
+				@Query("u") String username, @Query("p") String password);
+
+		@DELETE("/db/{database}/continuous_queries/{id}")
+		public String deleteContinuousQuery(@Path("database") String database, @Path("id") int id,
+				@Query("u") String username, @Query("p") String password);
+
+	}
+
+}
---
+++ src/test/java/org/influxdb/InfluxDBTest.java
@@ -0,0 +1,277 @@
+package org.influxdb;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDB.ContinuousQuery;
+import org.influxdb.InfluxDB.Database;
+import org.influxdb.InfluxDB.LogLevel;
+import org.influxdb.InfluxDB.Pong;
+import org.influxdb.InfluxDB.Serie;
+import org.influxdb.InfluxDB.User;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.google.common.base.Stopwatch;
+
+/**
+ * Test the InfluxDB API.
+ * 
+ * @author stefan.majer [at] gmail.com
+ * 
+ */
+@Test
+public class InfluxDBTest {
+
+	private InfluxDB influxDB;
+
+	@BeforeClass
+	public void setUp() {
+		this.influxDB = InfluxDB.connect("http://172.17.0.2:8086", "root", "root");
+		this.influxDB.setLogLevel(LogLevel.FULL);
+	}
+
+	/**
+	 * Ensure all Databases created get dropped afterwards.
+	 * 
+	 */
+	@AfterClass
+	public void tearDown() {
+		List<Database> result = this.influxDB.describeDatabases();
+		for (Database database : result) {
+			this.influxDB.deleteDatabase(database.getName());
+		}
+	}
+
+	@Test
+	public void pingTest() {
+		Pong result = this.influxDB.ping();
+		Assert.assertNotNull(result);
+		Assert.assertEquals(result.getStatus(), "ok");
+	}
+
+	@Test
+	public void describeDatabasesTest() {
+		String dbName = "unittest-" + System.currentTimeMillis();
+		String response = this.influxDB.createDatabase(dbName, 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+		List<Database> result = this.influxDB.describeDatabases();
+		Assert.assertNotNull(result);
+		Assert.assertTrue(result.size() > 0);
+		for (Database database : result) {
+			System.out.println(database.getName() + " " + database.getReplicationFactor());
+		}
+	}
+
+	@Test
+	public void createDatabasesTest() {
+		String dbName = "unittest-" + System.currentTimeMillis();
+		String response = this.influxDB.createDatabase(dbName, 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void deleteDatabasesTest() {
+		String response = this.influxDB.createDatabase("toDelete", 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+		response = this.influxDB.deleteDatabase("toDelete");
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+
+		// Creation of the same database must succeed.
+		response = this.influxDB.createDatabase("toDelete", 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+		response = this.influxDB.deleteDatabase("toDelete");
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+
+		List<Database> result = this.influxDB.describeDatabases();
+		for (Database database : result) {
+			System.out.println(database.getName() + " " + database.getReplicationFactor());
+		}
+	}
+
+	@Test
+	public void writeTest() {
+		String dbName = "write-unittest-" + System.currentTimeMillis();
+		String response = this.influxDB.createDatabase(dbName, 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+
+		Serie serie = new Serie();
+		serie.setName("testSeries");
+		serie.setColumns(new String[] { "value1", "value2" });
+		Object[] point = new Object[] { System.currentTimeMillis(), 5 };
+		serie.setPoints(new Object[][] { point });
+		Serie[] series = new Serie[] { serie };
+		this.influxDB.write(dbName, series, TimeUnit.MILLISECONDS);
+
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test()
+	public void writeManyTest() {
+		String dbName = "writemany-unittest-" + System.currentTimeMillis();
+		String response = this.influxDB.createDatabase(dbName, 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+		int outer = 20;
+		int inner = 50;
+		Stopwatch watch = new Stopwatch().start();
+		for (int i = 0; i < outer; i++) {
+			Serie serie = new Serie();
+			serie.setName("testSeries");
+			serie.setColumns(new String[] { "value1", "value2" });
+			Object[][] points = new Object[inner][2];
+			for (int j = 0; j < inner; j++) {
+				Object[] point = new Object[2];
+				point[0] = System.currentTimeMillis();
+				point[1] = 50f + (Math.random() * 10);
+				points[j] = point;
+			}
+			serie.setPoints(points);
+			Serie[] series = new Serie[] { serie };
+			this.influxDB.write(dbName, series, TimeUnit.MILLISECONDS);
+		}
+		System.out.println("Inserted " + (outer * inner) + " Datapoints in " + watch);
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void queryTest() {
+		String dbName = "query-unittest-" + System.currentTimeMillis();
+		String response = this.influxDB.createDatabase(dbName, 1);
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response, "ok");
+
+		Serie serie = new Serie();
+		serie.setName("testSeries");
+		serie.setColumns(new String[] { "value1", "value2" });
+		Object[] point = new Object[] { System.currentTimeMillis(), 5 };
+		serie.setPoints(new Object[][] { point });
+
+		Serie[] series = new Serie[] { serie };
+		this.influxDB.write(dbName, series, TimeUnit.MILLISECONDS);
+
+		List<Serie> result = this.influxDB.Query(dbName, "select value2 from testSeries", TimeUnit.MILLISECONDS);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(result.size(), 1);
+		// [{"name":"testSeries","columns":["time","sequence_number","value"],"points":[[1398412802823,160001,5]]}]
+		Assert.assertEquals((Double) result.get(0).getPoints()[0][2], 5d, 0d);
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void testCreateDescribeDeleteClusterAdmin() {
+		List<User> admins = this.influxDB.describeClusterAdmins();
+		int adminCount = admins.size();
+		this.influxDB.createClusterAdmin("aUser", "aPassword");
+		admins = this.influxDB.describeClusterAdmins();
+		Assert.assertNotNull(admins);
+		Assert.assertTrue(admins.size() == adminCount + 1);
+		this.influxDB.deleteClusterAdmin("aUser");
+		admins = this.influxDB.describeClusterAdmins();
+		Assert.assertNotNull(admins);
+		Assert.assertTrue(admins.size() == adminCount);
+	}
+
+	@Test
+	public void testUpdateClusterAdmin() {
+		this.influxDB.createClusterAdmin("aAdmin", "aPassword");
+		this.influxDB.updateClusterAdmin("aAdmin", "aNewPassword");
+		this.influxDB.deleteClusterAdmin("aAdmin");
+	}
+
+	@Test
+	public void testCreateDescribeDeleteDatabaseUser() {
+		String dbName = "createuser-unittest-" + System.currentTimeMillis();
+		this.influxDB.createDatabase(dbName, 1);
+		List<User> users = this.influxDB.describeDatabaseUsers(dbName);
+		int userCount = users.size();
+		this.influxDB.createDatabaseUser(dbName, "aUser", "aPassword");
+		users = this.influxDB.describeDatabaseUsers(dbName);
+		Assert.assertNotNull(users);
+		Assert.assertTrue(users.size() == userCount + 1);
+		this.influxDB.deleteDatabaseUser(dbName, "aUser");
+		users = this.influxDB.describeDatabaseUsers(dbName);
+		Assert.assertNotNull(users);
+		Assert.assertTrue(users.size() == userCount);
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void testUpdateAlterDatabaseUser() {
+		String dbName = "updateuser-unittest-" + System.currentTimeMillis();
+		this.influxDB.createDatabase(dbName, 1);
+		this.influxDB.createDatabaseUser(dbName, "aUser", "aPassword");
+		this.influxDB.updateDatabaseUser(dbName, "aUser", "aNewPassword");
+		this.influxDB.alterDatabasePrivilege(dbName, "aUser", true);
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void testUpdateAlterDatabaseUserWithPermissions() {
+		String dbName = "updateuserpermission-unittest-" + System.currentTimeMillis();
+		this.influxDB.createDatabase(dbName, 1);
+		this.influxDB.createDatabaseUser(dbName, "aUser", "aPassword", "^$", dbName);
+		this.influxDB.alterDatabasePrivilege(dbName, "aUser", true, "^$", dbName);
+
+		try {
+			this.influxDB.createDatabaseUser(dbName, "aUser", "aPassword", "^$");
+			Assert.fail("It is expected that createDatabaseUser fails with the wrong amount of permissions.");
+		} catch (Exception e) {
+			// Expected
+		}
+		try {
+			this.influxDB.alterDatabasePrivilege(dbName, "aUser", true, "^$", dbName, "invalid");
+			Assert.fail("It is expected that alterDatabasePrivilege fails with the wrong amount of permissions.");
+		} catch (Exception e) {
+			// Expected
+		}
+
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void testAuthenticateDatabaseUser() {
+		String dbName = "authenticateuser-unittest-" + System.currentTimeMillis();
+		this.influxDB.createDatabase(dbName, 1);
+		this.influxDB.createDatabaseUser(dbName, "aUser", "aPassword");
+		this.influxDB.updateDatabaseUser(dbName, "aUser", "aNewPassword");
+		this.influxDB.authenticateDatabaseUser(dbName, "aUser", "aNewPassword");
+		try {
+			this.influxDB.authenticateDatabaseUser(dbName, "aUser", "aWrongPassword");
+			Assert.fail("It is expected that authenticateDataBaseUser with a wrong password fails.");
+		} catch (Exception e) {
+			// Expected
+		}
+		this.influxDB.deleteDatabase(dbName);
+	}
+
+	@Test
+	public void testContinuousQueries() {
+		String dbName = "continuousquery-unittest-" + System.currentTimeMillis();
+		this.influxDB.createDatabase(dbName, 1);
+		this.influxDB.Query(dbName, "select * from clicks into events.global;", TimeUnit.MILLISECONDS);
+
+		List<ContinuousQuery> result = this.influxDB.getContinuousQueries(dbName);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(result.size(), 1);
+
+		this.influxDB.deleteContinuousQuery(dbName, result.get(0).getId());
+		result = this.influxDB.getContinuousQueries(dbName);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(result.size(), 0);
+
+		this.influxDB.deleteDatabase(dbName);
+	}
+}
