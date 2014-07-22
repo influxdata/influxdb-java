35c99cc059130550b449aa360d387c389d7088cb
Stefan Majer <stefan.majer@gmail.com>
2014-07-22 15:12:22 +0200

    Fix for #13 use ms instead of m for milliseconds timeprecision.

--- src/main/java/org/influxdb/impl/InfluxDBImpl.java
+++ src/main/java/org/influxdb/impl/InfluxDBImpl.java
@@ -276,7 +276,7 @@ public class InfluxDBImpl implements InfluxDB {
 		case SECONDS:
 			return "s";
 		case MILLISECONDS:
-			return "m";
+			return "ms";
 		case MICROSECONDS:
 			return "u";
 		default:
