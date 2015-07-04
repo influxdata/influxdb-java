b3c3cad668645373ad9b4f12b2a50f097d245fa4
Sumant Munjal <smunjal@gmail.com>
2015-07-04 12:19:19 -0700

    Added to return tags in QueryResult and added methods to accepts fields and tags for points

--- src/main/java/org/influxdb/dto/Point.java
+++ src/main/java/org/influxdb/dto/Point.java
@@ -18,237 +18,261 @@ import com.google.common.escape.Escapers;
  * 
  */
 public class Point {
-	private String measurement;
-	private Map<String, String> tags;
-	private Long time;
-	private TimeUnit precision = TimeUnit.NANOSECONDS;
-	private Map<String, Object> fields;
+    private String measurement;
+    private Map<String, String> tags;
+    private Long time;
+    private TimeUnit precision = TimeUnit.NANOSECONDS;
+    private Map<String, Object> fields;
 
-	private static final Escaper FIELD_ESCAPER = Escapers.builder().addEscape('"', "\\\"").build();
-	private static final Escaper KEY_ESCAPER = Escapers.builder().addEscape(' ', "\\ ").addEscape(',', "\\,").build();
+    private static final Escaper FIELD_ESCAPER = Escapers.builder().addEscape('"', "\\\"").build();
+    private static final Escaper KEY_ESCAPER = Escapers.builder().addEscape(' ', "\\ ").addEscape(',', "\\,").build();
 
-	Point() {
-	}
+    Point() {}
 
-	/**
-	 * Create a new Point Build build to create a new Point in a fluent manner-
-	 *
-	 * @param measurement
-	 *            the name of the measurement.
-	 * @return the Builder to be able to add further Builder calls.
-	 */
+    /**
+     * Create a new Point Build build to create a new Point in a fluent manner-
+     *
+     * @param measurement
+     *            the name of the measurement.
+     * @return the Builder to be able to add further Builder calls.
+     */
 
-	public static Builder measurement(final String measurement) {
-		return new Builder(measurement);
-	}
+    public static Builder measurement(final String measurement) {
+        return new Builder(measurement);
+    }
 
-	/**
-	 * Builder for a new Point.
-	 *
-	 * @author stefan.majer [at] gmail.com
-	 *
-	 */
-	public static final class Builder {
-		private final String measurement;
-		private final Map<String, String> tags = Maps.newTreeMap(Ordering.natural());
-		private Long time;
-		private TimeUnit precision = TimeUnit.NANOSECONDS;
-		private final Map<String, Object> fields = Maps.newTreeMap(Ordering.natural());
+    /**
+     * Builder for a new Point.
+     *
+     * @author stefan.majer [at] gmail.com
+     *
+     */
+    public static final class Builder {
+        private final String measurement;
+        private final Map<String, String> tags = Maps.newTreeMap(Ordering.natural());
+        private Long time;
+        private TimeUnit precision = TimeUnit.NANOSECONDS;
+        private final Map<String, Object> fields = Maps.newTreeMap(Ordering.natural());
 
-		/**
-		 * @param measurement
-		 */
-		Builder(final String measurement) {
-			this.measurement = measurement;
-		}
+        /**
+         * @param measurement
+         */
+        Builder(final String measurement) {
+            this.measurement = measurement;
+        }
 
-		/**
-		 * Add a tag to this point.
-		 *
-		 * @param tagName
-		 *            the tag name
-		 * @param value
-		 *            the tag value
-		 * @return the Builder instance.
-		 */
-		public Builder tag(final String tagName, final String value) {
-			this.tags.put(tagName, value);
-			return this;
-		}
+        /**
+         * Add a tag to this point.
+         *
+         * @param tagName
+         *            the tag name
+         * @param value
+         *            the tag value
+         * @return the Builder instance.
+         */
+        public Builder tag(final String tagName, final String value) {
+            this.tags.put(tagName, value);
+            return this;
+        }
 
-		/**
-		 * Add a field to this point.
-		 *
-		 * @param field
-		 *            the field name
-		 * @param value
-		 *            the value of this field
-		 * @return the Builder instance.
-		 */
-		public Builder field(final String field, final Object value) {
-			this.fields.put(field, value);
-			return this;
-		}
+        /**
+         * Add a Map of tags to this point.
+         *
+         * @param tags
+         *            the map of fields
+         * @return the Builder instance.
+         */
+        public Builder tags(final Map<String, String> tags) {
+            this.tags.putAll(tags);
+            return this;
+        }
 
-		/**
-		 * Add a time to this point
-		 *
-		 * @param precisionToSet
-		 * @param timeToSet
-		 * @return the Builder instance.
-		 */
-		public Builder time(final long timeToSet, final TimeUnit precisionToSet) {
-			this.time = timeToSet;
-			if (null != precisionToSet) {
-				this.precision = precisionToSet;
-			}
-			return this;
-		}
+        /**
+         * Add a field to this point.
+         *
+         * @param field
+         *            the field name
+         * @param value
+         *            the value of this field
+         * @return the Builder instance.
+         */
+        public Builder field(final String field, final Object value) {
+            this.fields.put(field, value);
+            return this;
+        }
 
-		/**
-		 * Create a new Point.
-		 *
-		 * @return the newly created Point.
-		 */
-		public Point build() {
-			Preconditions
-					.checkArgument(!Strings.isNullOrEmpty(this.measurement), "Point name must not be null or empty.");
-			Preconditions.checkArgument(this.fields.size() > 0, "Point must have at least one field specified.");
-			Point point = new Point();
-			point.setFields(this.fields);
-			point.setMeasurement(this.measurement);
-			point.setPrecision(this.precision);
-			point.setTags(this.tags);
-			point.setTime(this.time);
-			return point;
-		}
-	}
+        /**
+         * Add a Map of fields to this point.
+         *
+         * @param fields
+         *            the map of fields
+         * @return the Builder instance.
+         */
+        public Builder fields(final Map<String, Object> fields) {
+            this.fields.putAll(fields);
+            return this;
+        }
 
-	/**
-	 * @param measurement
-	 *            the measurement to set
-	 */
-	void setMeasurement(final String measurement) {
-		this.measurement = measurement;
-	}
+        /**
+         * Add a time to this point
+         *
+         * @param precisionToSet
+         * @param timeToSet
+         * @return the Builder instance.
+         */
+        public Builder time(final long timeToSet, final TimeUnit precisionToSet) {
+            this.time = timeToSet;
+            if (null != precisionToSet) {
+                this.precision = precisionToSet;
+            }
+            return this;
+        }
 
-	/**
-	 * @param time
-	 *            the time to set
-	 */
-	void setTime(final Long time) {
-		this.time = time;
-	}
+        /**
+         * Create a new Point.
+         *
+         * @return the newly created Point.
+         */
+        public Point build() {
+            Preconditions
+                    .checkArgument(!Strings.isNullOrEmpty(this.measurement), "Point name must not be null or empty.");
+            Preconditions.checkArgument(this.fields.size() > 0, "Point must have at least one field specified.");
+            Point point = new Point();
+            point.setFields(this.fields);
+            point.setMeasurement(this.measurement);
+            point.setPrecision(this.precision);
+            point.setTags(this.tags);
+            point.setTime(this.time);
+            return point;
+        }
+    }
 
-	/**
-	 * @param tags
-	 *            the tags to set
-	 */
-	void setTags(final Map<String, String> tags) {
-		this.tags = tags;
-	}
+    /**
+     * @param measurement
+     *            the measurement to set
+     */
+    void setMeasurement(final String measurement) {
+        this.measurement = measurement;
+    }
 
-	/**
-	 * @return the tags
-	 */
-	Map<String, String> getTags() {
-		return this.tags;
-	}
+    /**
+     * @param time
+     *            the time to set
+     */
+    void setTime(final Long time) {
+        this.time = time;
+    }
 
-	/**
-	 * @param precision
-	 *            the precision to set
-	 */
-	void setPrecision(final TimeUnit precision) {
-		this.precision = precision;
-	}
+    /**
+     * @param tags
+     *            the tags to set
+     */
+    void setTags(final Map<String, String> tags) {
+        this.tags = tags;
+    }
 
-	/**
-	 * @param fields
-	 *            the fields to set
-	 */
-	void setFields(final Map<String, Object> fields) {
-		this.fields = fields;
-	}
+    /**
+     * @return the tags
+     */
+    Map<String, String> getTags() {
+        return this.tags;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Point [name=");
-		builder.append(this.measurement);
-		builder.append(", time=");
-		builder.append(this.time);
-		builder.append(", tags=");
-		builder.append(this.tags);
-		builder.append(", precision=");
-		builder.append(this.precision);
-		builder.append(", fields=");
-		builder.append(this.fields);
-		builder.append("]");
-		return builder.toString();
-	}
+    /**
+     * @param precision
+     *            the precision to set
+     */
+    void setPrecision(final TimeUnit precision) {
+        this.precision = precision;
+    }
 
-	/**
-	 * calculate the lineprotocol entry for a single Point.
-	 * 
-	 * Documentation is WIP : https://github.com/influxdb/influxdb/pull/2997
-	 * 
-	 * https://github.com/influxdb/influxdb/blob/master/tsdb/README.md
-	 *
-	 * @return the String without newLine.
-	 */
-	public String lineProtocol() {
-		final StringBuilder sb = new StringBuilder();
-		sb.append(KEY_ESCAPER.escape(this.measurement));
-		sb.append(concatenatedTags());
-		sb.append(concatenateFields());
-		sb.append(formatedTime());
-		return sb.toString();
-	}
+    /**
+     * @param fields
+     *            the fields to set
+     */
+    void setFields(final Map<String, Object> fields) {
+        this.fields = fields;
+    }
 
-	private StringBuilder concatenatedTags() {
-		final StringBuilder sb = new StringBuilder();
-		for (Entry<String, String> tag : this.tags.entrySet()) {
-			sb.append(",");
-			sb.append(KEY_ESCAPER.escape(tag.getKey())).append("=").append(KEY_ESCAPER.escape(tag.getValue()));
-		}
-		sb.append(" ");
-		return sb;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Point [name=");
+        builder.append(this.measurement);
+        builder.append(", time=");
+        builder.append(this.time);
+        builder.append(", tags=");
+        builder.append(this.tags);
+        builder.append(", precision=");
+        builder.append(this.precision);
+        builder.append(", fields=");
+        builder.append(this.fields);
+        builder.append("]");
+        return builder.toString();
+    }
 
-	private StringBuilder concatenateFields() {
-		final StringBuilder sb = new StringBuilder();
-		final int fieldCount = this.fields.size();
-		int loops = 0;
+    /**
+     * calculate the lineprotocol entry for a single Point.
+     * 
+     * Documentation is WIP : https://github.com/influxdb/influxdb/pull/2997
+     * 
+     * https://github.com/influxdb/influxdb/blob/master/tsdb/README.md
+     *
+     * @return the String without newLine.
+     */
+    public String lineProtocol() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(KEY_ESCAPER.escape(this.measurement));
+        sb.append(concatenatedTags());
+        sb.append(concatenateFields());
+        sb.append(formatedTime());
+        return sb.toString();
+    }
 
-		for (Entry<String, Object> field : this.fields.entrySet()) {
-			sb.append(KEY_ESCAPER.escape(field.getKey())).append("=");
-			loops++;
-			Object value = field.getValue();
-			if (value instanceof String) {
-				String stringValue = (String) value;
-				sb.append("\"").append(FIELD_ESCAPER.escape(stringValue)).append("\"");
-			} else {
-				sb.append(value);
-			}
+    private StringBuilder concatenatedTags() {
+        final StringBuilder sb = new StringBuilder();
+        for (Entry<String, String> tag : this.tags.entrySet()) {
+            sb.append(",");
+            sb.append(KEY_ESCAPER.escape(tag.getKey())).append("=").append(KEY_ESCAPER.escape(tag.getValue()));
+        }
+        sb.append(" ");
+        return sb;
+    }
 
-			if (loops < fieldCount) {
-				sb.append(",");
-			}
-		}
-		return sb;
-	}
+    private StringBuilder concatenateFields() {
+        final StringBuilder sb = new StringBuilder();
+        final int fieldCount = this.fields.size();
+        int loops = 0;
 
-	private StringBuilder formatedTime() {
-		final StringBuilder sb = new StringBuilder();
-		if (null == this.time) {
-			this.time = System.nanoTime();
-		}
-		sb.append(" ").append(TimeUnit.NANOSECONDS.convert(this.time, this.precision));
-		return sb;
-	}
+        for (Entry<String, Object> field : this.fields.entrySet()) {
+            sb.append(KEY_ESCAPER.escape(field.getKey())).append("=");
+            loops++;
+            Object value = field.getValue();
+            if (value instanceof String) {
+                String stringValue = (String)value;
+                sb.append("\"").append(FIELD_ESCAPER.escape(stringValue)).append("\"");
+            }
+            else {
+                sb.append(value);
+            }
+
+            if (loops < fieldCount) {
+                sb.append(",");
+            }
+        }
+        return sb;
+    }
+
+    private StringBuilder formatedTime() {
+        final StringBuilder sb = new StringBuilder();
+        if (null == this.time) {
+            this.time = System.nanoTime();
+        }
+        sb.append(" ").append(TimeUnit.NANOSECONDS.convert(this.time, this.precision));
+        return sb;
+    }
 
 }
--- src/main/java/org/influxdb/dto/QueryResult.java
+++ src/main/java/org/influxdb/dto/QueryResult.java
@@ -1,6 +1,10 @@
 package org.influxdb.dto;
 
 import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
 
 /**
  * {Purpose of This Type}
@@ -24,169 +28,184 @@ import java.util.List;
 // {"results":[{"series":[{"name":"databases","columns":["name"],"values":[["mydb"]]}]}]}
 public class QueryResult {
 
-	private List<Result> results;
-	private String error;
+    private List<Result> results;
+    private String error;
 
-	/**
-	 * @return the results
-	 */
-	public List<Result> getResults() {
-		return this.results;
-	}
+    /**
+     * @return the results
+     */
+    public List<Result> getResults() {
+        return this.results;
+    }
 
-	/**
-	 * @param results
-	 *            the results to set
-	 */
-	public void setResults(final List<Result> results) {
-		this.results = results;
-	}
+    /**
+     * @param results
+     *            the results to set
+     */
+    public void setResults(final List<Result> results) {
+        this.results = results;
+    }
 
-	/**
-	 * @return the error
-	 */
-	public String getError() {
-		return this.error;
-	}
+    /**
+     * @return the error
+     */
+    public String getError() {
+        return this.error;
+    }
 
-	/**
-	 * @param error
-	 *            the error to set
-	 */
-	public void setError(final String error) {
-		this.error = error;
-	}
+    /**
+     * @param error
+     *            the error to set
+     */
+    public void setError(final String error) {
+        this.error = error;
+    }
 
-	public static class Result {
-		private List<Series> series;
-		private String error;
+    public static class Result {
+        private List<Series> series;
+        private String error;
 
-		/**
-		 * @return the series
-		 */
-		public List<Series> getSeries() {
-			return this.series;
-		}
+        /**
+         * @return the series
+         */
+        public List<Series> getSeries() {
+            return this.series;
+        }
 
-		/**
-		 * @param series
-		 *            the series to set
-		 */
-		public void setSeries(final List<Series> series) {
-			this.series = series;
-		}
+        /**
+         * @param series
+         *            the series to set
+         */
+        public void setSeries(final List<Series> series) {
+            this.series = series;
+        }
 
-		/**
-		 * @return the error
-		 */
-		public String getError() {
-			return this.error;
-		}
+        /**
+         * @return the error
+         */
+        public String getError() {
+            return this.error;
+        }
 
-		/**
-		 * @param error
-		 *            the error to set
-		 */
-		public void setError(final String error) {
-			this.error = error;
-		}
+        /**
+         * @param error
+         *            the error to set
+         */
+        public void setError(final String error) {
+            this.error = error;
+        }
 
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			builder.append("Result [series=");
-			builder.append(this.series);
-			builder.append(", error=");
-			builder.append(this.error);
-			builder.append("]");
-			return builder.toString();
-		}
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Result [series=");
+            builder.append(this.series);
+            builder.append(", error=");
+            builder.append(this.error);
+            builder.append("]");
+            return builder.toString();
+        }
 
-	}
+    }
 
-	public static class Series {
-		private String name;
-		private List<String> columns;
-		private List<List<Object>> values;
+    public static class Series {
+        private String name;
+        private List<String> columns;
+        private List<List<Object>> values;
+        private Map<String, String> tags = Maps.newTreeMap(Ordering.natural());
 
-		/**
-		 * @return the name
-		 */
-		public String getName() {
-			return this.name;
-		}
+        /**
+         * @return the name
+         */
+        public String getName() {
+            return this.name;
+        }
 
-		/**
-		 * @param name
-		 *            the name to set
-		 */
-		public void setName(final String name) {
-			this.name = name;
-		}
+        /**
+         * @param name
+         *            the name to set
+         */
+        public void setName(final String name) {
+            this.name = name;
+        }
 
-		/**
-		 * @return the columns
-		 */
-		public List<String> getColumns() {
-			return this.columns;
-		}
+        /**
+         * @return the columns
+         */
+        public List<String> getColumns() {
+            return this.columns;
+        }
 
-		/**
-		 * @param columns
-		 *            the columns to set
-		 */
-		public void setColumns(final List<String> columns) {
-			this.columns = columns;
-		}
+        /**
+         * @param columns
+         *            the columns to set
+         */
+        public void setColumns(final List<String> columns) {
+            this.columns = columns;
+        }
 
-		/**
-		 * @return the values
-		 */
-		public List<List<Object>> getValues() {
-			return this.values;
-		}
+        /**
+         * @return the values
+         */
+        public List<List<Object>> getValues() {
+            return this.values;
+        }
 
-		/**
-		 * @param values
-		 *            the values to set
-		 */
-		public void setValues(final List<List<Object>> values) {
-			this.values = values;
-		}
+        /**
+         * @param values
+         *            the values to set
+         */
+        public void setValues(final List<List<Object>> values) {
+            this.values = values;
+        }
 
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			builder.append("Series [name=");
-			builder.append(this.name);
-			builder.append(", columns=");
-			builder.append(this.columns);
-			builder.append(", values=");
-			builder.append(this.values);
-			builder.append("]");
-			return builder.toString();
-		}
+        /**
+         * @param values
+         *            the values to set
+         */
+        public void setTags(final Map<String, String> tags) {
+            this.tags = tags;
+        }
 
-	}
+        public Map<String, String> getTags() {
+            return tags;
+        }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("QueryResult [results=");
-		builder.append(this.results);
-		builder.append(", error=");
-		builder.append(this.error);
-		builder.append("]");
-		return builder.toString();
-	}
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Series [name=");
+            builder.append(this.name);
+            builder.append(", tags=");
+            builder.append(this.tags);
+            builder.append(", columns=");
+            builder.append(this.columns);
+            builder.append(", values=");
+            builder.append(this.values);
+            builder.append("]");
+            return builder.toString();
+        }
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("QueryResult [results=");
+        builder.append(this.results);
+        builder.append(", error=");
+        builder.append(this.error);
+        builder.append("]");
+        return builder.toString();
+    }
 
 }
