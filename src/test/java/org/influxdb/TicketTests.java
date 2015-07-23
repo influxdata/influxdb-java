0c384bd4db613451cb0e164af8c172be35a577b1
Stefan Majer <stefan.majer@gmail.com>
2015-07-23 14:52:05 +0200

    Adoption of tests to changes related to value precision.

--- src/test/java/org/influxdb/TicketTests.java
+++ src/test/java/org/influxdb/TicketTests.java
@@ -1,13 +1,7 @@
 package org.influxdb;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -148,76 +142,4 @@ public class TicketTests {
 		this.influxDB.deleteDatabase(dbName);
 	}
 
-	/**
-	 * Test for ticket #44
-	 */
-	@Test(enabled = true)
-	public void testTicket44() {
-		Point point = Point.measurement("test").time(1, TimeUnit.MICROSECONDS).field("a", 1).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=1 1000");
-
-		point = Point.measurement("test").time(1, TimeUnit.MILLISECONDS).field("a", 1).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=1 1000000");
-
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", 1).build();
-		BatchPoints batchPoints = BatchPoints.database("db").point(point).build();
-		assertThat(batchPoints.lineProtocol()).asString().isEqualTo("test a=1 1\n");
-
-		point = Point.measurement("test").time(1, TimeUnit.MICROSECONDS).field("a", 1).build();
-		batchPoints = BatchPoints.database("db").point(point).build();
-		assertThat(batchPoints.lineProtocol()).asString().isEqualTo("test a=1 1000\n");
-
-		point = Point.measurement("test").time(1, TimeUnit.MILLISECONDS).field("a", 1).build();
-		batchPoints = BatchPoints.database("db").point(point).build();
-		assertThat(batchPoints.lineProtocol()).asString().isEqualTo("test a=1 1000000\n");
-
-		point = Point.measurement("test").field("a", 1).time(1, TimeUnit.MILLISECONDS).build();
-		batchPoints = BatchPoints.database("db").build();
-		batchPoints = batchPoints.point(point);
-		assertThat(batchPoints.lineProtocol()).asString().isEqualTo("test a=1 1000000\n");
-
-	}
-
-	@Test
-	public void testTicket54() {
-		Byte byteNumber = 100;
-		Point point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", byteNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100 1");
-
-		int intNumber = 100000000;
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", intNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100000000 1");
-
-		Integer integerNumber = 100000000;
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", integerNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100000000 1");
-
-		AtomicInteger atomicIntegerNumber = new AtomicInteger(100000000);
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", atomicIntegerNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100000000 1");
-
-		Long longNumber = 1000000000000000000L;
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", longNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=1000000000000000000 1");
-
-		AtomicLong atomicLongNumber = new AtomicLong(1000000000000000000L);
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", atomicLongNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=1000000000000000000 1");
-
-		BigInteger bigIntegerNumber = BigInteger.valueOf(100000000);
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", bigIntegerNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100000000 1");
-
-		Double doubleNumber = Double.valueOf(100000000.0001);
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", doubleNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100000000.0001 1");
-
-		Float floatNumber = Float.valueOf(0.1f);
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", floatNumber).build();
-		assertThat(point.lineProtocol()).asString().startsWith("test a=0.10");
-
-		BigDecimal bigDecimalNumber = BigDecimal.valueOf(100000000.00000001);
-		point = Point.measurement("test").time(1, TimeUnit.NANOSECONDS).field("a", bigDecimalNumber).build();
-		assertThat(point.lineProtocol()).asString().isEqualTo("test a=100000000.00000001 1");
-	}
 }
