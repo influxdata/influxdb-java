adab333b5850386b424af2966969498970a475d9
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2015-12-05 21:24:28 +0100

    Fix for issue #117.

--- src/main/java/org/influxdb/dto/Point.java
+++ src/main/java/org/influxdb/dto/Point.java
@@ -286,9 +286,13 @@ public class Point {
 		numberFormat.setMinimumFractionDigits(1);
 
 		for (Entry<String, Object> field : this.fields.entrySet()) {
-			sb.append(KEY_ESCAPER.escape(field.getKey())).append("=");
 			loops++;
 			Object value = field.getValue();
+			if (value == null) {
+				continue;
+			}
+
+			sb.append(KEY_ESCAPER.escape(field.getKey())).append("=");
 			if (value instanceof String) {
 				String stringValue = (String) value;
 				sb.append("\"").append(FIELD_ESCAPER.escape(stringValue)).append("\"");
@@ -304,6 +308,7 @@ public class Point {
 				sb.append(",");
 			}
 		}
+
 		return sb;
 	}
 
