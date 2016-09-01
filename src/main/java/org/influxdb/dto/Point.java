5ac187135a61aa388e53f4e1fefa58c7d201d95d
Hani Naguib <hani@gimbal.com>
2016-09-01 00:44:38 -0700

    issue 140

--- src/main/java/org/influxdb/dto/Point.java
+++ src/main/java/org/influxdb/dto/Point.java
@@ -343,9 +343,10 @@ public class Point {
 	}
 
 	private StringBuilder formatedTime() {
+		Preconditions.checkArgument(this.precision != null, "Precision must not be null.");
 		final StringBuilder sb = new StringBuilder();
 		if (null == this.time) {
-			this.time = System.nanoTime();
+			this.time = this.precision.convert(System.currentTimeMillis(),TimeUnit.MILLISECONDS);
 		}
 		sb.append(" ").append(TimeUnit.NANOSECONDS.convert(this.time, this.precision));
 		return sb;
