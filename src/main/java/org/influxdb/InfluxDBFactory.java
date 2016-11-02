a06957d0df0edb99d3354e343b3f1371640d403c
stroller <fujian1115@gmail.com>
2016-11-02 08:59:27 +0800

    support creating instance without username and password

--- src/main/java/org/influxdb/InfluxDBFactory.java
+++ src/main/java/org/influxdb/InfluxDBFactory.java
@@ -15,7 +15,21 @@ import okhttp3.OkHttpClient;
  * 
  */
 public enum InfluxDBFactory {
+	
 	;
+	
+	/**
+	 * Create a connection to a InfluxDB.
+	 * 
+	 * @param url
+	 *            the url to connect to.
+	 * @return a InfluxDB adapter suitable to access a InfluxDB.
+	 */
+	public static InfluxDB connect(final String url) {
+		Preconditions.checkArgument(!Strings.isNullOrEmpty(url), "The URL may not be null or empty.");
+		return new InfluxDBImpl(url, null, null, new OkHttpClient.Builder());
+	}
+	
 	/**
 	 * Create a connection to a InfluxDB.
 	 * 
@@ -33,7 +47,21 @@ public enum InfluxDBFactory {
 		Preconditions.checkArgument(!Strings.isNullOrEmpty(username), "The username may not be null or empty.");
 		return new InfluxDBImpl(url, username, password, new OkHttpClient.Builder());
 	}
-
+	
+	/**
+	 * Create a connection to a InfluxDB.
+	 * 
+	 * @param url
+	 *            the url to connect to.
+	 * @param client
+	 * 			  the HTTP client to use
+	 * @return a InfluxDB adapter suitable to access a InfluxDB.
+	 */
+	public static InfluxDB connect(final String url, final OkHttpClient.Builder client) {
+		Preconditions.checkArgument(!Strings.isNullOrEmpty(url), "The URL may not be null or empty.");
+		Preconditions.checkNotNull(client, "The client may not be null.");
+		return new InfluxDBImpl(url, null, null, client);
+	}
 
 	/**
 	 * Create a connection to a InfluxDB.
