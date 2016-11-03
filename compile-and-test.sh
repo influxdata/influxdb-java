f8349c263839a83d2480857ab781d7594139ec07
Stefan Majer <stefan.majer@f-i-ts.de>
2016-11-03 21:27:35 +0100

    Run tests against 3 versions of influxdb, use alpine based docker image for smaller images

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -2,16 +2,23 @@
 #
 # script to start influxdb and compile influxdb-java with all tests.
 #
+set -e
 
-docker rm influxdb
-docker run -d --name influxdb -p 8086:8086 influxdb
+INFLUXDB_VERSIONS="0.13-alpine 1.0-alpine 1.1.0-rc1-alpine"
 
-docker run -it --rm  \
-       -v $PWD:/usr/src/mymaven \
-       -v $PWD/.m2:/root/.m2 \
-       -w /usr/src/mymaven \
-       --link=influxdb \
-       -e INFLUXDB_IP=influxdb \
-       maven mvn clean install
+for version in ${INFLUXDB_VERSIONS}
+do
+  echo "Tesing againts influxdb ${version}"
+  docker rm influxdb
+  docker run -d --name influxdb -p 8086:8086 influxdb:${version}
 
-docker kill influxdb
+  docker run -it --rm  \
+         -v $PWD:/usr/src/mymaven \
+         -v $PWD/.m2:/root/.m2 \
+         -w /usr/src/mymaven \
+         --link=influxdb \
+         -e INFLUXDB_IP=influxdb \
+         maven mvn clean install
+
+  docker kill influxdb
+done
