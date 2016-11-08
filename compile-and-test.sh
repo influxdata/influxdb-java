6b3e1eebf9f39bda58f7ba6f6e702221b04d8510
Stefan Majer <stefan.majer@f-i-ts.de>
2016-11-08 10:29:41 +0100

    Start influxdb with udp enabled and published, related to #226

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -11,14 +11,20 @@ do
   echo "Tesing againts influxdb ${version}"
   docker kill influxdb || true
   docker rm influxdb || true
-  docker run -d --name influxdb -p 8086:8086 influxdb:${version}
+  docker run \
+            --detach \
+            --name influxdb \
+            --publish 8086:8086 \
+            --publish 8089:8089/udp \
+            --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
+        influxdb:${version}
 
   docker run -it --rm  \
-         -v $PWD:/usr/src/mymaven \
-         -v $PWD/.m2:/root/.m2 \
-         -w /usr/src/mymaven \
-         --link=influxdb \
-         -e INFLUXDB_IP=influxdb \
+        --volume $PWD:/usr/src/mymaven \
+        --volume $PWD/.m2:/root/.m2 \
+        --workdir /usr/src/mymaven \
+        --link=influxdb \
+        --env INFLUXDB_IP=influxdb \
          maven mvn clean install
 
   docker kill influxdb || true
