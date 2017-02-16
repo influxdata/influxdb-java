671d5ac69b434e9f37158910698463858292658f
Tom Lee <github@tomlee.co>
2017-02-15 19:46:12 -0800

    Ensure BatchProcessor threads get properly cleaned up in tests

--- src/test/java/org/influxdb/impl/BatchProcessorTest.java
+++ src/test/java/org/influxdb/impl/BatchProcessorTest.java
@@ -24,23 +24,27 @@ public class BatchProcessorTest {
         InfluxDB mockInfluxDB = mock(InfluxDBImpl.class);
         BatchProcessor batchProcessor = BatchProcessor.builder(mockInfluxDB).actions(Integer.MAX_VALUE)
             .interval(1, TimeUnit.NANOSECONDS).build();
+        try {
+            doThrow(new RuntimeException()).when(mockInfluxDB).write(any(BatchPoints.class));
 
-        doThrow(new RuntimeException()).when(mockInfluxDB).write(any(BatchPoints.class));
+            Point point = Point.measurement("cpu").field("6", "").build();
+            BatchProcessor.HttpBatchEntry batchEntry1 = new BatchProcessor.HttpBatchEntry(point, "db1", "");
+            BatchProcessor.HttpBatchEntry batchEntry2 = new BatchProcessor.HttpBatchEntry(point, "db2", "");
 
-        Point point = Point.measurement("cpu").field("6", "").build();
-        BatchProcessor.HttpBatchEntry batchEntry1 = new BatchProcessor.HttpBatchEntry(point, "db1", "");
-        BatchProcessor.HttpBatchEntry batchEntry2 = new BatchProcessor.HttpBatchEntry(point, "db2", "");
+            batchProcessor.put(batchEntry1);
+            Thread.sleep(200); // wait for scheduler
 
-        batchProcessor.put(batchEntry1);
-        Thread.sleep(200); // wait for scheduler
+            // first try throws an exception
+            verify(mockInfluxDB, times(1)).write(any(BatchPoints.class));
 
-        // first try throws an exception
-        verify(mockInfluxDB, times(1)).write(any(BatchPoints.class));
-
-        batchProcessor.put(batchEntry2);
-        Thread.sleep(200); // wait for scheduler
-        // without try catch the 2nd time does not occur
-        verify(mockInfluxDB, times(2)).write(any(BatchPoints.class));
+            batchProcessor.put(batchEntry2);
+            Thread.sleep(200); // wait for scheduler
+            // without try catch the 2nd time does not occur
+            verify(mockInfluxDB, times(2)).write(any(BatchPoints.class));
+        }
+        finally {
+            batchProcessor.flush();
+        }
     }
 
     @Test
@@ -49,16 +53,21 @@ public class BatchProcessorTest {
         BatchProcessor batchProcessor = BatchProcessor.builder(mockInfluxDB).actions(Integer.MAX_VALUE)
             .interval(1, TimeUnit.NANOSECONDS).build();
 
-        Point point = Point.measurement("cpu").field("6", "").build();
-        BatchProcessor.HttpBatchEntry batchEntry1 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_1");
-        BatchProcessor.HttpBatchEntry batchEntry2 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_2");
+        try {
+            Point point = Point.measurement("cpu").field("6", "").build();
+            BatchProcessor.HttpBatchEntry batchEntry1 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_1");
+            BatchProcessor.HttpBatchEntry batchEntry2 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_2");
 
-        batchProcessor.put(batchEntry1);
-        batchProcessor.put(batchEntry2);
+            batchProcessor.put(batchEntry1);
+            batchProcessor.put(batchEntry2);
 
-        Thread.sleep(200); // wait for scheduler
-        // same dbname with different rp should write two batchs instead of only one.
-        verify(mockInfluxDB, times(2)).write(any(BatchPoints.class));
+            Thread.sleep(200); // wait for scheduler
+            // same dbname with different rp should write two batchs instead of only one.
+            verify(mockInfluxDB, times(2)).write(any(BatchPoints.class));
+        }
+        finally {
+            batchProcessor.flush();
+        }
     }
 
     @Test
@@ -67,36 +76,39 @@ public class BatchProcessorTest {
         BatchProcessor batchProcessor = BatchProcessor.builder(mockInfluxDB).actions(2)
             .interval(10, TimeUnit.MINUTES).build();
 
-        Point point = Point.measurement("cpu").addField("4", "").build();
-        BatchProcessor.HttpBatchEntry batchEntry1 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_1");
-        BatchProcessor.HttpBatchEntry batchEntry2 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_2");
-        BatchProcessor.HttpBatchEntry batchEntry3 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_3");
+        try {
+            Point point = Point.measurement("cpu").addField("4", "").build();
+            BatchProcessor.HttpBatchEntry batchEntry1 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_1");
+            BatchProcessor.HttpBatchEntry batchEntry2 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_2");
+            BatchProcessor.HttpBatchEntry batchEntry3 = new BatchProcessor.HttpBatchEntry(point, "db1", "rp_3");
 
-        assertThat(batchProcessor.offer(batchEntry1), is(true));
-        assertThat(batchProcessor.offer(batchEntry2), is(true));
-        assertThat(batchProcessor.offer(batchEntry3), is(false));
-
-        batchProcessor.flush();
+            assertThat(batchProcessor.offer(batchEntry1), is(true));
+            assertThat(batchProcessor.offer(batchEntry2), is(true));
+            assertThat(batchProcessor.offer(batchEntry3), is(false));
+        }
+        finally {
+            batchProcessor.flush();
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testActionsIsZero() throws InterruptedException, IOException {
         InfluxDB mockInfluxDB = mock(InfluxDBImpl.class);
         BatchProcessor.builder(mockInfluxDB).actions(0)
-            .interval(1, TimeUnit.NANOSECONDS).build();
+            .interval(1, TimeUnit.NANOSECONDS).build().flush();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testIntervalIsZero() throws InterruptedException, IOException {
         InfluxDB mockInfluxDB = mock(InfluxDBImpl.class);
         BatchProcessor.builder(mockInfluxDB).actions(1)
-            .interval(0, TimeUnit.NANOSECONDS).build();
+            .interval(0, TimeUnit.NANOSECONDS).build().flush();
     }
 
     @Test(expected = NullPointerException.class)
     public void testInfluxDBIsNull() throws InterruptedException, IOException {
         InfluxDB mockInfluxDB = null;
         BatchProcessor.builder(mockInfluxDB).actions(1)
-            .interval(1, TimeUnit.NANOSECONDS).build();
+            .interval(1, TimeUnit.NANOSECONDS).build().flush();
     }
 }
