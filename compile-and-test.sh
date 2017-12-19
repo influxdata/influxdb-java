2887f8f0278c4be56fd76055b413ee55359f0100
Artur Keska <artur.keska@xdsnet.pl>
2017-12-20 00:09:00 +0100

    test: added tests for testing connectivity over nginx as a proxy
    - changed compile-and-test.sh scripts, so it starts both standard and "proxy"
    - added simple nginx config for docker container
    - tests related to UDP communication moved to separate class and skipped in case of http proxy testing

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -18,24 +18,17 @@
 
 set -e
 
-#USE_PROXY=nginx
-#INFLUXDB_VERSIONS="1.4 1.3 1.2 1.1"
-#JAVA_VERSIONS="3-jdk-8-alpine 3-jdk-9-slim"
 
-INFLUXDB_VERSIONS="1.4"
-JAVA_VERSIONS="3-jdk-8-alpine"
+INFLUXDB_VERSIONS="1.4 1.3 1.2 1.1"
+JAVA_VERSIONS="3-jdk-8-alpine 3-jdk-9-slim"
 
 WORKDIR=/usr/src/mymaven
 
 if [ -z "$BUILD_HOME" ] ; then
-  BUILD_HOME=$PWD
-fi
-
-INFLUXDB_API_URL=http://influxdb:8086
-
-if [ "$USE_PROXY" == "nginx" ] ; then
-    echo Test with Nginx as proxy
-    INFLUXDB_API_URL=http://nginx:80/influx-api
+    BUILD_HOME=$PWD
+    if [ -x  /c/Windows/System32/ ] ; then
+	BUILD_HOME=/$PWD
+    fi
 fi
 
 if [ -x  /c/Windows/System32/ ] ; then
@@ -45,47 +38,67 @@ fi
 
 echo Using build home: $BUILD_HOME
 
+function run_test {
+    USE_PROXY=$1
+    
+    INFLUXDB_API_URL=http://influxdb:8086
+    if [ "$USE_PROXY" == "nginx" ] ; then
+	echo Test with Nginx as proxy
+	INFLUXDB_API_URL=http://nginx:8080/influx-api/
+    fi
 
-for java_version in ${JAVA_VERSIONS}
-do
- echo "Run tests with maven:${java_version}"
-for version in ${INFLUXDB_VERSIONS}
-do
-  echo "Tesing againts influxdb ${version}"
-  docker kill influxdb || true
-  docker rm influxdb || true
-  docker pull influxdb:${version}-alpine || true
-  docker run \
-            --detach \
-            --name influxdb \
-            --publish 8086:8086 \
-            --publish 8089:8089/udp \
-            --volume ${BUILD_HOME}/influxdb.conf:/etc/influxdb/influxdb.conf \
-        influxdb:${version}-alpine
+    
+    for java_version in ${JAVA_VERSIONS}
+    do
+	echo "Run tests with maven:${java_version}"
+	for version in ${INFLUXDB_VERSIONS}
+	do
+	    echo "Tesing againts influxdb ${version}"
+	    docker kill influxdb || true
+	    docker rm influxdb || true
+	    docker pull influxdb:${version}-alpine || true
+	    docker run \
+		   --detach \
+		   --name influxdb \
+		   --publish 8086:8086 \
+		   --publish 8089:8089/udp \
+		   --volume ${BUILD_HOME}/influxdb.conf:/etc/influxdb/influxdb.conf \
+		   influxdb:${version}-alpine
 
- if [ "$USE_PROXY" == "nginx" ] ; then
-     echo Starting Nginx
-     docker rm -f nginx || true
-     docker run \
-            --detach \
-            --name nginx \
-            --publish 8888:80 \
-            --volume ${BUILD_HOME}/src/test/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
-            --link influxdb:influxdb \
-            nginx nginx-debug '-g' 'daemon off;'
-     NGINX_LINK=--link=nginx
- fi
- 
- docker run -it --rm  \
-        --volume $BUILD_HOME:/usr/src/mymaven \
-        --volume $BUILD_HOME/.m2:/root/.m2 \
-        --workdir $WORKDIR \
-        --link=influxdb $NGINX_LINK \
-        --env INFLUXDB_API_URL=$INFLUXDB_API_URL \
-         maven:${java_version} mvn clean install
+	    if [ "$USE_PROXY" == "nginx" ] ; then
+		echo Starting Nginx
+		docker kill nginx || true		
+		docker rm  nginx || true
+		echo ----- STARTING NGINX CONTAINER -----
+		docker run \
+		       --detach \
+		       --name nginx \
+		       --publish 8888:8080 \
+		       --volume ${BUILD_HOME}/src/test/nginx/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+		       --link influxdb:influxdb \
+		       nginx nginx '-g' 'daemon off;'
+		
+		NGINX_LINK=--link=nginx
+		SKIP_TESTS=-DsomeModule.test.excludes="**/*UDPInfluxDBTest*"
+	    fi
+	    
+	    docker run -it --rm  \
+		   --volume $BUILD_HOME:/usr/src/mymaven \
+		   --volume $BUILD_HOME/.m2:/root/.m2 \
+		   --workdir $WORKDIR \
+		   --link=influxdb $NGINX_LINK \
+		   --env INFLUXDB_API_URL=$INFLUXDB_API_URL \
+		   maven:${java_version} mvn clean install $SKIP_TESTS
 
- docker kill influxdb || true
- docker kill nginx || true
- docker rm -f nginx || true
-done
-done
+	    docker kill influxdb || true
+	    docker kill nginx || true
+	    docker rm -f nginx || true
+	done
+    done
+}
+
+################################################################################
+################################################################################
+
+run_test
+run_test nginx
--- src/test/java/org/influxdb/InfluxDBTest.java
+++ src/test/java/org/influxdb/InfluxDBTest.java
@@ -2,7 +2,6 @@ package org.influxdb;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -36,7 +35,6 @@ import org.junit.runner.RunWith;
 public class InfluxDBTest {
 
     private InfluxDB influxDB;
-    private final static int UDP_PORT = 8089;
     private final static String UDP_DATABASE = "udp";
 
     /**
@@ -188,64 +186,7 @@ public class InfluxDBTest {
         Assertions.assertFalse(result.getResults().get(0).getSeries().get(0).getTags().isEmpty());
         this.influxDB.deleteDatabase(dbName);
     }
-
-    /**
-     * Test the implementation of {@link InfluxDB#write(int, Point)}'s sync
-     * support.
-     */
-    @Test
-    public void testSyncWritePointThroughUDP() throws InterruptedException {
-        this.influxDB.disableBatch();
-        String measurement = TestUtils.getRandomMeasurement();
-        Point point = Point.measurement(measurement).tag("atag", "test").addField("used", 80L).addField("free", 1L).build();
-        this.influxDB.write(UDP_PORT, point);
-        Thread.sleep(2000);
-        Query query = new Query("SELECT * FROM " + measurement + " GROUP BY *", UDP_DATABASE);
-        QueryResult result = this.influxDB.query(query);
-        Assertions.assertFalse(result.getResults().get(0).getSeries().get(0).getTags().isEmpty());
-    }
-
-    /**
-     * Test the implementation of {@link InfluxDB#write(int, Point)}'s async
-     * support.
-     */
-    @Test
-    public void testAsyncWritePointThroughUDP() throws InterruptedException {
-        this.influxDB.enableBatch(1, 1, TimeUnit.SECONDS);
-        try {
-            Assertions.assertTrue(this.influxDB.isBatchEnabled());
-            String measurement = TestUtils.getRandomMeasurement();
-            Point point = Point.measurement(measurement).tag("atag", "test").addField("used", 80L).addField("free", 1L).build();
-            this.influxDB.write(UDP_PORT, point);
-            Thread.sleep(2000);
-            Query query = new Query("SELECT * FROM " + measurement + " GROUP BY *", UDP_DATABASE);
-            QueryResult result = this.influxDB.query(query);
-            Assertions.assertFalse(result.getResults().get(0).getSeries().get(0).getTags().isEmpty());
-        } finally {
-            this.influxDB.disableBatch();
-        }
-    }
-
-    /**
-     * Test the implementation of {@link InfluxDB#write(int, Point)}'s async
-     * support.
-     */
-    @Test
-    public void testAsyncWritePointThroughUDPFail() {
-        this.influxDB.enableBatch(1, 1, TimeUnit.SECONDS);
-        try {
-            Assertions.assertTrue(this.influxDB.isBatchEnabled());
-            String measurement = TestUtils.getRandomMeasurement();
-            Point point = Point.measurement(measurement).tag("atag", "test").addField("used", 80L).addField("free", 1L).build();
-            Thread.currentThread().interrupt();
-            Assertions.assertThrows(RuntimeException.class, () -> {
-                this.influxDB.write(UDP_PORT, point);
-            });
-        } finally {
-            this.influxDB.disableBatch();
-        }
-    }
-
+    
     /**
      * Test writing to the database using string protocol.
      */
@@ -279,91 +220,7 @@ public class InfluxDBTest {
         this.influxDB.deleteDatabase(dbName);
     }
 
-    /**
-     * Test writing to the database using string protocol through UDP.
-     */
-    @Test
-    public void testWriteStringDataThroughUDP() throws InterruptedException {
-        String measurement = TestUtils.getRandomMeasurement();
-        this.influxDB.write(UDP_PORT, measurement + ",atag=test idle=90,usertime=9,system=1");
-        //write with UDP may be executed on server after query with HTTP. so sleep 2s to handle this case
-        Thread.sleep(2000);
-        Query query = new Query("SELECT * FROM " + measurement + " GROUP BY *", UDP_DATABASE);
-        QueryResult result = this.influxDB.query(query);
-        Assertions.assertFalse(result.getResults().get(0).getSeries().get(0).getTags().isEmpty());
-    }
-
-    /**
-     * Test writing multiple records to the database using string protocol
-     * through UDP.
-     */
-    @Test
-    public void testWriteMultipleStringDataThroughUDP() throws InterruptedException {
-        String measurement = TestUtils.getRandomMeasurement();
-        this.influxDB.write(UDP_PORT, measurement + ",atag=test1 idle=100,usertime=10,system=1\n"
-                + measurement + ",atag=test2 idle=200,usertime=20,system=2\n"
-                + measurement + ",atag=test3 idle=300,usertime=30,system=3");
-        Thread.sleep(2000);
-        Query query = new Query("SELECT * FROM " + measurement + " GROUP BY *", UDP_DATABASE);
-        QueryResult result = this.influxDB.query(query);
-
-        Assertions.assertEquals(3, result.getResults().get(0).getSeries().size());
-        Assertions.assertEquals("test1", result.getResults().get(0).getSeries().get(0).getTags().get("atag"));
-        Assertions.assertEquals("test2", result.getResults().get(0).getSeries().get(1).getTags().get("atag"));
-        Assertions.assertEquals("test3", result.getResults().get(0).getSeries().get(2).getTags().get("atag"));
-    }
-
-    /**
-     * Test writing multiple separate records to the database using string
-     * protocol through UDP.
-     */
-    @Test
-    public void testWriteMultipleStringDataLinesThroughUDP() throws InterruptedException {
-        String measurement = TestUtils.getRandomMeasurement();
-        this.influxDB.write(UDP_PORT, Arrays.asList(
-                measurement + ",atag=test1 idle=100,usertime=10,system=1",
-                measurement + ",atag=test2 idle=200,usertime=20,system=2",
-                measurement + ",atag=test3 idle=300,usertime=30,system=3"
-        ));
-        Thread.sleep(2000);
-        Query query = new Query("SELECT * FROM " + measurement + " GROUP BY *", UDP_DATABASE);
-        QueryResult result = this.influxDB.query(query);
-
-        Assertions.assertEquals(3, result.getResults().get(0).getSeries().size());
-        Assertions.assertEquals("test1", result.getResults().get(0).getSeries().get(0).getTags().get("atag"));
-        Assertions.assertEquals("test2", result.getResults().get(0).getSeries().get(1).getTags().get("atag"));
-        Assertions.assertEquals("test3", result.getResults().get(0).getSeries().get(2).getTags().get("atag"));
-    }
-
-    /**
-     * When batch of points' size is over UDP limit, the expected exception is
-     * java.lang.RuntimeException: java.net.SocketException: The message is
-     * larger than the maximum supported by the underlying transport: Datagram
-     * send failed
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testWriteMultipleStringDataLinesOverUDPLimit() throws Exception {
-        //prepare data
-        List<String> lineProtocols = new ArrayList<String>();
-        int i = 0;
-        int length = 0;
-        while (true) {
-            Point point = Point.measurement("udp_single_poit").addField("v", i).build();
-            String lineProtocol = point.lineProtocol();
-            length += (lineProtocol.getBytes("utf-8")).length;
-            lineProtocols.add(lineProtocol);
-            if (length > 65535) {
-                break;
-            }
-        }
-        //write batch of string which size is over 64K
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            this.influxDB.write(UDP_PORT, lineProtocols);
-        });
-    }
-
+  
     /**
      * Test writing multiple records to the database using string protocol.
      */
