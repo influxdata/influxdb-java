9b200f03f49d06d7bec4b5e53348ce2cbc867a0e
Tomas Klapka <tomas.klapka@bonitoo.io>
2018-02-27 11:06:14 +0100

    final solution for running the release

--- run_influxdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-            --detach \
-            --name influxdb \
-            --publish 8086:8086 \
-            --publish 8089:8089/udp \
-            --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
-	influxdb:latest
