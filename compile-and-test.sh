eb84f007f815260ea33b2d5ec0e79f2a05dbdeca
Ulrich Metzger <u.metzger@e-brokers.de>
2018-05-13 14:51:54 +0200

    fix for merge conflicts with conflicting file compile-and-test.sh

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -4,51 +4,45 @@
 #
 set -e
 
-INFLUXDB_VERSIONS="1.4 1.3 1.2 1.1"
+DEFAULT_INFLUXDB_VERSION="1.5"
+DEFAULT_MAVEN_JAVA_VERSION="3-jdk-10-slim"
 
-JAVA_VERSIONS="3-jdk-8-alpine 3-jdk-9-slim"
+INFLUXDB_VERSION="${INFLUXDB_VERSION:-$DEFAULT_INFLUXDB_VERSION}"
+MAVEN_JAVA_VERSION="${MAVEN_JAVA_VERSION:-$DEFAULT_MAVEN_JAVA_VERSION}"
 
+echo "Run tests with maven:${MAVEN_JAVA_VERSION} on onfluxdb-${INFLUXDB_VERSION}"
 
-for java_version in ${JAVA_VERSIONS}
-do
- echo "Run tests with maven:${java_version}"
-for version in ${INFLUXDB_VERSIONS}
-do
-  echo "Tesing againts influxdb ${version}"
-  docker kill influxdb || true
-  docker rm influxdb || true
-  docker kill nginx || true
-  docker rm nginx || true
+docker kill influxdb || true
+docker rm influxdb || true
+docker kill nginx || true
+docker rm nginx || true
+docker pull influxdb:${version}-alpine || true
+docker run \
+          --detach \
+          --name influxdb \
+          --publish 8086:8086 \
+          --publish 8089:8089/udp \
+          --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
+      influxdb:${INFLUXDB_VERSION}-alpine
 
-  docker pull influxdb:${version}-alpine || true
-  docker run \
-            --detach \
-            --name influxdb \
-            --publish 8086:8086 \
-            --publish 8089:8089/udp \
-            --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
-        influxdb:${version}-alpine
+docker run  \
+        --detach \
+        --name nginx\
+         --volume ${PWD}/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+         --volume ${PWD}/.htpasswd:/etc/nginx/.htpasswd:ro \
+         --publish 80:80 \
+         --link=influxdb \
+         nginx
 
-  docker run  \
-            --detach \
-            --name nginx\
-             --volume ${PWD}/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
-             --volume ${PWD}/.htpasswd:/etc/nginx/.htpasswd:ro \
-             --publish 80:80 \
-             --link=influxdb \
-             nginx
+docker run -it --rm  \
+      --volume $PWD:/usr/src/mymaven \
+      --volume $PWD/.m2:/root/.m2 \
+      --workdir /usr/src/mymaven \
+      --link=influxdb \
+      --link=nginx \
+      --env INFLUXDB_IP=influxdb \
+      --env PROXY_IP=nginx \
+        maven:${MAVEN_JAVA_VERSION} mvn clean install
 
-  docker run -it --rm  \
-        --volume $PWD:/usr/src/mymaven \
-        --volume $PWD/.m2:/root/.m2 \
-        --workdir /usr/src/mymaven \
-        --link=influxdb \
-        --link=nginx \
-        --env INFLUXDB_IP=influxdb \
-        --env PROXY_IP=nginx \
-         maven:${java_version} mvn clean install
-
-  docker kill influxdb || true
-  docker kill nginx || true
-done
-done
+docker kill influxdb || true
+docker kill nginx || true
