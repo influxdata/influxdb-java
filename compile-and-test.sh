078144e9c35f7353c5097f35fea7c2f07879be23
Stefan Majer <stefan.majer@f-i-ts.de>
2018-07-13 16:27:36 +0200

    Use much small nginx:alpine image, formatting and typos

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -2,7 +2,7 @@
 #
 # script to start influxdb and compile influxdb-java with all tests.
 #
-set -e
+set -ex
 
 DEFAULT_INFLUXDB_VERSION="1.6"
 DEFAULT_MAVEN_JAVA_VERSION="3-jdk-10-slim"
@@ -10,23 +10,22 @@ DEFAULT_MAVEN_JAVA_VERSION="3-jdk-10-slim"
 INFLUXDB_VERSION="${INFLUXDB_VERSION:-$DEFAULT_INFLUXDB_VERSION}"
 MAVEN_JAVA_VERSION="${MAVEN_JAVA_VERSION:-$DEFAULT_MAVEN_JAVA_VERSION}"
 
-echo "Run tests with maven:${MAVEN_JAVA_VERSION} on onfluxdb-${INFLUXDB_VERSION}"
+echo "Run tests with maven:${MAVEN_JAVA_VERSION} on influxdb-${INFLUXDB_VERSION}"
 docker kill influxdb || true
 docker rm influxdb || true
-docker pull influxdb:${version}-alpine || true
+docker pull influxdb:${INFLUXDB_VERSION}-alpine || true
 docker run \
-   --detach \
-   --name influxdb \
-   --publish 8086:8086 \
-   --publish 8089:8089/udp \
-          --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
-   influxdb:${INFLUXDB_VERSION}-alpine
+       --detach \
+       --name influxdb \
+       --publish 8086:8086 \
+       --publish 8089:8089/udp \
+       --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
+       influxdb:${INFLUXDB_VERSION}-alpine
 
-echo Starting Nginx
-docker kill nginx || true        
+echo "Starting Nginx"
+docker kill nginx || true
 docker rm  nginx || true
 
-echo ----- STARTING NGINX CONTAINER -----
 docker run \
        --detach \
        --name nginx \
@@ -34,21 +33,22 @@ docker run \
        --publish 8080:8080/udp \
        --volume ${PWD}/src/test/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
        --link influxdb:influxdb \
-       nginx:stable nginx '-g' 'daemon off;'
+       nginx:stable-alpine nginx '-g' 'daemon off;'
 
+echo "Running tests"
 PROXY_API_URL=http://nginx:8080/influx-api/
 PROXY_UDP_PORT=8080
-       
+
 docker run -it --rm  \
-      --volume $PWD:/usr/src/mymaven \
-      --volume $PWD/.m2:/root/.m2 \
-      --workdir /usr/src/mymaven \
-   --link=influxdb \
-   --link=nginx \
-   --env INFLUXDB_IP=influxdb \
-   --env PROXY_API_URL=$PROXY_API_URL \
-   --env PROXY_UDP_PORT=$PROXY_UDP_PORT \
-   maven:${MAVEN_JAVA_VERSION} mvn clean install
+       --volume ${PWD}:/usr/src/mymaven \
+       --volume ${PWD}/.m2:/root/.m2 \
+       --workdir /usr/src/mymaven \
+       --link=influxdb \
+       --link=nginx \
+       --env INFLUXDB_IP=influxdb \
+       --env PROXY_API_URL=${PROXY_API_URL} \
+       --env PROXY_UDP_PORT=${PROXY_UDP_PORT} \
+       maven:${MAVEN_JAVA_VERSION} mvn clean install
 
 docker kill influxdb || true
 docker kill nginx || true
