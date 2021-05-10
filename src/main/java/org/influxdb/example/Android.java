4e1724f3cf7c1fe1b42f045054cb68da4c7e0455
Strakar <cedric.baloche@gmail.com>
2021-05-10 11:28:52 +0200

    Update Android.java
    Update for Checklist

--- src/main/java/org/influxdb/example/Android.java
+++ src/main/java/org/influxdb/example/Android.java
@@ -9,79 +9,75 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 
  * @author StrakarCe
  * @since 07/05/2021
  * @version 1
  */
 public class Android {
-
-
-	String address = "http://192.168.1.75:8000/"; // put the address IP of your database
+	// put the address IP of your database
+	String address = "http://192.168.1.75:8000/";
 	String dbName = "myDatabase";
 	String table = "SERIES";
 	QueryResult actual;
-	Boolean flag = false; 
+	Boolean flag = false;
 	InfluxDB con;
-	
+
 	public Android() {
 		super();
 	}
-	
-	 public void queryExecute(Query query){
+	 public void queryExecute(final Query query) {
 	        Thread thread = new Thread(new Runnable() {
 
 	            @Override
 	            public void run() {
 	                try  {
-	                	//InfluxDB connector = InfluxDBFactory.connect(address); // if you want to open every time 
-	                    System.out.println("Send the query to the database ..."); // FOR A REAL APP CREATE A LOGGER ;)
+	                	//InfluxDB connector = InfluxDBFactory.connect(address);
+				// if you want to open every time
+	                    System.out.println("Send the query to the database ...");
+				// FOR A REAL APP CREATE A LOGGER ;
 	                    List<QueryResult> results = new LinkedList<>();
 	                    actual = con.query(query);
 	                } catch (Exception e) {
 	                    e.printStackTrace();
 	                }
-	                flag = true; // For simplicity, I use a simple flag to know when the thread have finished 
+	                flag = true; // For simplicity, I use a simple flag to know when the thread have finished
 	            }
 	        });
 
 	        thread.start();
 	    }
 
-	 	/**
-	 	 * It's to open the connexion with the database. In my case I decide to open once, do many query and close. 
-	 	 */
+		/**
+		* It's to open the connexion with the database.
+		* In my case I decide to open once, do many query and close.
+		*/
 	    public void connexion() {
 	        con = InfluxDBFactory.connect(address);
 	    }
 	    /**
-	     * It's to close after my  list of query 
+	     * It's to close after my  list of query.
 	     */
 	    public void close() {
 	        con.close();
 	    }
-	        
-	    
-	    /* 
-	     * simple example of how you can create a query 
+	    /*
+	     * simple example of how you can create a query
 	     */
-	    private void queryLauncher(String query) {
+	    private void queryLauncher(final String query) {
 	    	queryExecute(new Query(query, dbName));
-	        while(flag!=true) { // ugly method to wait the thread 
-
+	        while (!flag) { // ugly method to wait the thread
+			System.out.println("Wait the thread");
 	        }
-	        flag= false;
+	        flag = false;
 	    }
 	    public String getEtat() {
 	        queryLauncher("select last(value) from PTEC");
 	        return actual.getResults().get(0).getSeries().get(0).getValues().get(0).get(1).toString();
 	    }
-	    
 	    public String getHC() {
 	    	queryLauncher("SELECT last(value) FROM HCHC");
 	        return actual.getResults().get(0).getSeries().get(0).getValues().get(0).get(1).toString();
 	    }
-	    
 	    // ------------------------- Example when you want to use it ------------
 	    /*
 	    Android test = new Android();
